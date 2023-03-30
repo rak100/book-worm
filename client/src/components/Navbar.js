0d7f15c6ac8200fc4e4f981c262b367d3c3ac79a
rak10 <ahmadus10@icloud.com>
2023-03-30 22:50:34 +0500

    added navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,19 +15,19 @@ const AppNavbar = () => {
       <Navbar bg='danger' variant='danger' expand='lg'>
         <Container fluid>
           <Navbar.Brand as={Link} to='/'>
-            Google Books Search
+            Book Worm Search Engine
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
             <Nav className='ml-auto d-flex'>
               <Nav.Link as={Link} to='/'>
-                Search For Books
+                Search Books
               </Nav.Link>
               {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
                 <>
                   <Nav.Link as={Link} to='/saved'>
-                    See Your Books
+                    See Your Favourite Books
                   </Nav.Link>
                   <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                 </>
